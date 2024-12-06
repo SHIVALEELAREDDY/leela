557480ed7fa8447990ee61b466d5c751a3cb107c
SHIVALEELAREDDY <itsshivureddy956@gmail.com>
2024-12-06 19:44:58 +0530

    file created

---
+++ index.js
@@ -0,0 +1,52 @@
+//  var userinput=prompt("emter your precentage")
+if(18) {
+  console.log("your are eligible for vote");
+}else{
+    console.log("your are not eligble for the vote ");
+    
+}
+var boy=21;
+if(21){
+    console.log("your are eligible for marriage");
+    
+}else{
+    console.log("your are not  eligible for marriage");
+    
+}
+var girl=18;
+if(18){
+    console.log("your are eligible for marriage");
+    
+}else{
+    console.log("your are not eligible for marriage");
+    
+}
+var driving=18;
+if(18){
+    console.log("your are eligible for Driving License");
+    
+}else{
+    console.log("your are not eligible for driving license");
+    
+}
+
+var exam=75;
+if(75){
+    console.log("your are eligible for exam eligibility");
+    
+}else{
+    console.log("your are not eligible for exam eligibility");
+    
+}
+// var userinput=prompt("enter your  Senior Citizen")
+if(60){
+    console.log("enter are eligible for senior citizen discount");
+    
+}else{
+    console.log("enter are not eligible for senior citizen discount");
+    
+}
+
+
+
+
